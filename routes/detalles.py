cb8ac42a6b5561c2c9184dc897b573c13c9a1e9a
AngelX2000 <miguel_osorio11@hotmail.com>
2025-06-05 19:55:17 -0500

    avanze de detalles

---
+++ routes/detalles.py
@@ -0,0 +1,14 @@
+import os
+import requests
+from flask import Blueprint, render_template
+from dotenv import load_dotenv
+
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+detalles_bp=Blueprint("Detalles", __name__)
+
+@detalles_bp.route("/detalles")
+def detalles():
+    url = f"https://api.themoviedb.org/3/movie/550?api_key={API_KEY}&language=es-ES"
