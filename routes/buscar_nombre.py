f593595ace505d1c36677275468b97e7636c89ce
carolincamacho <carolincamacho@javerianacali.edu.co>
2025-06-05 20:11:05 -0500

    colaboracion

--- routes/buscar_nombre.py
+++ routes/buscar_nombre.py
@@ -11,7 +11,8 @@ busqueda_bp = Blueprint("busqueda", __name__)
 
 @busqueda_bp.route("/busqueda")
 def busqueda():
-    url = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query=batman&language=es-ES"
+    #buscar=request.form.get("buscar")
+    url = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query=stich&language=es-ES"
     response = requests.get(url)
     datos = response.json()["results"]
-    return render_template("busqueda.html", datos=datos)
+    return render_template("buscar_nombre.html", datos=datos)
