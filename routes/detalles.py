bdf5a3279803830a7e0bf79caec9bfaa0c0ae76a
AngelX2000 <miguel_osorio11@hotmail.com>
2025-06-05 19:56:01 -0500

    avanze de ruta

--- routes/detalles.py
+++ routes/detalles.py
@@ -12,3 +12,5 @@ detalles_bp=Blueprint("Detalles", __name__)
 @detalles_bp.route("/detalles")
 def detalles():
     url = f"https://api.themoviedb.org/3/movie/550?api_key={API_KEY}&language=es-ES"
+    response = requests.get(url)
+    genero = id
