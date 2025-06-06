66c4997f7a0e244f59c3d69218a1433d3b155c8e
alejandro314 <manuelmartines385@gmail.com>
2025-06-05 21:30:08 -0500

    actualizacion de main

--- routes/proximos.py
+++ routes/proximos.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from flask import Blueprint, render_template
-from dotenv import load_dotenv
+from dotenv import load_dotenv  
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
