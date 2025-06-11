23b963e277fd599342b8bcb9d1b5ca062890381c
juanjosalaz20 <juanjosalazar2000@gmail.com>
2025-06-10 20:22:45 -0500

    ESTILOS MEJOR VALORADAS

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from flask import Flask
 from routes.popular import popular_bp
 from routes.mejor_valoradas import mejor_valoradas_bp
-from routes.buscar_nombre import busqueda_bp
-from routes.detalles import detalles_bp
-from routes.proximos import proximos_bp
-from routes.generos import generos_bp
+#from routes.buscar_nombre import busqueda_bp
+#from routes.detalles import detalles_bp
+#from routes.proximos import proximos_bp
+#from routes.generos import generos_bp
 
 app=Flask(__name__)
 
 app.register_blueprint(popular_bp)
 app.register_blueprint(mejor_valoradas_bp)
-app.register_blueprint(busqueda_bp)
-app.register_blueprint(detalles_bp)
-app.register_blueprint(proximos_bp)
-app.register_blueprint(generos_bp)
+#app.register_blueprint(busqueda_bp)
+#app.register_blueprint(detalles_bp)
+#app.register_blueprint(proximos_bp)
+#app.register_blueprint(generos_bp)
 
 if __name__=="__main__":
     app.run(debug=True)
--- routes/mejor_valoradas.py
+++ routes/mejor_valoradas.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,4 +13,21 @@ def mejor_valoradas():
     url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={API_KEY}&language=es-ES&page=1"
     response = requests.get(url)
     datos = response.json()["results"]
+
+     # Leer filtros desde la URL
+    buscar = request.args.get("buscar", "").lower()
+    ordenar = request.args.get("ordenar", "titulo")
+
+    # Filtro
+    if buscar:
+        datos = [p for p in datos if buscar in p["title"].lower()]
+
+    # Ordenamiento
+    if ordenar == "titulo":
+        datos.sort(key=lambda x: x["title"])
+    elif ordenar == "año":
+        datos.sort(key=lambda x: x.get("release_date", "")[:4])
+    elif ordenar == "rating":
+        datos.sort(key=lambda x: x.get("vote_average", 0), reverse=True)
+
     return render_template("mejor_valoradas.html", datos=datos)
