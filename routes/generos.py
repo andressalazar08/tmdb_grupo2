ac1c35f95443c8a9b2041804637b1ded9a405129
jsdiaz27 <juan_s.diaz_v@uao.edu.co>
2025-06-10 20:21:23 -0500

    CAMBIO 1 EN GENEROS

--- routes/generos.py
+++ routes/generos.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,5 +12,13 @@ generos_bp = Blueprint("generos", __name__)
 def generos():
     url = f"https://api.themoviedb.org/3/genre/movie/list?api_key={API_KEY}&language=es-ES"
     response = requests.get(url)
-    datos = response.json()["results"]
+    datos = response.json()["genres"]
     return render_template("generos.html", datos=datos)
+
+
+@generos_bp.route("/peliculas_por_genero/<int:genero_id>")
+def peliculas_por_genero(genero_id):
+    url = f"https://api.themoviedb.org/3/discover/movie?api_key={API_KEY}&language=es-ES&with_genres={genero_id}"
+    response = requests.get(url)
+    peliculas = response.json().get("results", [])
+    return jsonify(peliculas)
