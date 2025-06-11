9130082f85adfbd7b2e0d654dad4962ef3f3849d
carolincamacho <carolincamacho@javerianacali.edu.co>
2025-06-10 20:22:22 -0500

    ejercio_jun

--- routes/buscar_nombre.py
+++ routes/buscar_nombre.py
@@ -1,7 +1,7 @@
 #ruta de carolina
 import os
 import requests
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,10 +9,12 @@ API_KEY = os.getenv("API_KEY")
 
 busqueda_bp = Blueprint("busqueda", __name__)
 
-@busqueda_bp.route("/busqueda")
+@busqueda_bp.route("/busqueda",methods=["GET","POST"])
 def busqueda():
-    #buscar=request.form.get("buscar")
-    url = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query=stich&language=es-ES"
-    response = requests.get(url)
-    datos = response.json()["results"]
-    return render_template("buscar_nombre.html", datos=datos)
+    if request.method=="POST":
+        buscar=request.form.get("buscar")
+        url = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={buscar}&language=es-ES"
+        response = requests.get(url)
+        datos = response.json()["results"]
+        return render_template("buscar_nombre.html", datos=datos)
+    return render_template("buscar_nombre.html")
